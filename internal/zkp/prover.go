bb878306114a64cbfc8e98bbe8dba87e9f7b6602
Uslje <147374020+uslje13@users.noreply.github.com>
2026-03-10 19:48:55 +0100

    chore: ignore ZKP key files for security

--- internal/zkp/prover.go
+++ internal/zkp/prover.go
@@ -7,6 +7,7 @@ import (
 	"github.com/consensys/gnark/constraint"
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/frontend/cs/r1cs"
+	"os"
 )
 
 type Engine struct {
@@ -21,13 +22,51 @@ func Setup() (*Engine, error) {
 	compiledConstraintSystem, err := frontend.Compile(ecc.BN254.ScalarField(), r1cs.NewBuilder, &circuit)
 
 	if err != nil {
-		return nil, fmt.Errorf("An error occured during compiling circuit: %w", err)
+		return nil, fmt.Errorf("an error occured during compiling circuit: %w", err)
 	}
 
 	provingKey, verifyingKey, err := groth16.Setup(compiledConstraintSystem)
 	if err != nil {
-		return nil, fmt.Errorf("An error occured during generating keys: %w", err)
+		return nil, fmt.Errorf("an error occured during generating keys: %w", err)
 	}
+
+	pkFile, err := os.Create("proving.key")
+	if err != nil {
+		return nil, fmt.Errorf("failed to create proving.key file: %w", err)
+	}
+
+	_, err = provingKey.WriteTo(pkFile)
+	if err != nil {
+		closeErr := pkFile.Close()
+		if closeErr != nil {
+			return nil, fmt.Errorf("failed to write proving key: %v (also failed to close file: %v)", err, closeErr)
+		}
+		return nil, fmt.Errorf("failed to write proving key: %w", err)
+	}
+
+	if err := pkFile.Close(); err != nil {
+		return nil, fmt.Errorf("failed to close proving key file: %w", err)
+	}
+	fmt.Println("[SETUP] Successfully saved proving.key")
+
+	vkFile, err := os.Create("verifying.key")
+	if err != nil {
+		return nil, fmt.Errorf("failed to create verifying.key file: %w", err)
+	}
+
+	_, err = verifyingKey.WriteTo(vkFile)
+	if err != nil {
+		closeErr := vkFile.Close()
+		if closeErr != nil {
+			return nil, fmt.Errorf("failed to write verifying key: %v (also failed to close file: %v)", err, closeErr)
+		}
+		return nil, fmt.Errorf("failed to write verifying key: %w", err)
+	}
+
+	if err := vkFile.Close(); err != nil {
+		return nil, fmt.Errorf("failed to close verifying key file: %w", err)
+	}
+	fmt.Println("[SETUP] Successfully saved verifying.key")
 
 	return &Engine{
 		ProvingKey:               provingKey,
